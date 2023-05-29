7bb5b1fa2338fb03d0f00730fd0ca158cefa80b5
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-05-29 17:38:46 +0300

    map

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ const App: React.FC = () => {
 
 const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
   const files = event.target.files;
+  console.log("Selected file:", files);
+
   if (files && files.length > 0) {
     const file = files[0];
 
@@ -14,34 +16,48 @@ const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
 
     reader.onload = (e: ProgressEvent<FileReader>) => {
       if (e.target && e.target.result) {
-        // Отримання бінарних даних з файлу
-        const data = e.target.result as ArrayBuffer;
-        // Розпарсити бінарні дані і оновити стан binaryData
-        const parsedData = parseBinaryData(data, 36000, 17999); // Застосувати розміри width і height
-        setBinaryData(parsedData);
+        try {
+          // Отримання бінарних даних з файлу
+          const data = e.target.result as ArrayBuffer;
+          // Розпарсити бінарні дані і оновити стан binaryData
+          const parsedData = parseBinaryData(data, 36000, 17999); // Застосувати розміри width і height
+          setBinaryData(parsedData);
+        } catch (error) {
+          console.error("Помилка:", error);
+          console.error("Стек викликів:", error.stack);
+          // Обробка помилки, якщо щось пішло не так під час розпарсування даних
+        }
       }
+    };
+
+    reader.onerror = (e: ProgressEvent<FileReader>) => {
+      console.error("Помилка при читанні файлу:", e.target?.error);
+      // Обробка помилки, якщо сталася помилка під час читання файлу
     };
 
     reader.readAsArrayBuffer(file);
   }
 };
-  return (
-    <div>
-      <input type="file" accept=".grid" onChange={handleFileUpload} />
-      {binaryData.length > 0 && (
-        <>
-          <img src="../images/emptyMap.jpeg" alt="map" />
-          <Heatmap
-            binaryData={binaryData}
-            minValue={0}
-            maxValue={255}
-            getColor={getColor}
-            height={binaryData.length}
-          />
-        </>
-      )}
-    </div>
-  );
+
+return (
+  <div>
+    <input type="file" accept=".grid" onChange={handleFileUpload} />
+    {binaryData.length > 0 && (
+      <Heatmap
+        binaryData={binaryData}
+        minValue={0}
+        maxValue={255}
+        getColor={getColor}
+        height={binaryData.length}
+      />
+    )}
+    <img
+      src="https://moweb.azureedge.net/careers/heat-map-task/empty-map.jpg"
+      alt="map"
+    />
+  </div>
+);
+
 };
 
 export default App;
